3ea1919fc8f645df0717a53e04b0662785f2a4ee
jwde <jaydestories@gmail.com>
2015-09-14 13:38:44 -0400

    updated content

--- js/terminal.js
+++ js/terminal.js
@@ -7,21 +7,28 @@ $(window).load(function() {
         + "Enjoys playing strategy games.<br>"
         + "Enjoys playing guitar.";
     var portfolioText = "<a href='http://piper.link'>piper</a> - image sharing and realtime discussion site<br>" +
-                        "<a href='http://fairfieldauction.com'>fairfieldauction</a> - antiques and fine art auction site";
+                        "<a href='http://fairfieldauction.com'>fairfieldauction</a> - antiques and fine art auction site<br>" +
+                        "<a href='https://github.com/jwde/Digital-Composer'>Digital Composer</a> - melody composer trained by humans on the web";
     var cvText = "Academics<br>"
                 + "Tufts University - School of Engineering, Medford, MA, expected diploma May 2017<br>"
-                + "&nbsp;&nbsp;&nbsp;&nbsp;Double-Majoring in Computer Science and Mathematics<br>"
-                + "&nbsp;&nbsp;&nbsp;&nbsp;Deans List - Fall 2013, Spring 2014, Fall 2014, Spring 2015<br>"
-                + "&nbsp;&nbsp;&nbsp;&nbsp;Overall GPA: 3.75<br>"
-                + "&nbsp;&nbsp;&nbsp;&nbsp;Major GPA: 3.91<br>"
-                + "&nbsp;&nbsp;&nbsp;&nbsp;Major Courses: Data Structures, Machine Structure and Assembly Language, Web Programming, Web Engineering, Programming Languages, Operating Systems, Computation Theory, Networks, Computer System Security, Visualization, Natural Language Processing, Intro Electrical Systems, Intro Digital Electronics, Discrete Math, Multivariate Calculus, Differential Equations, Abstract Linear Algebra, Probability<br><br>"
+                + "- Double-Majoring in Computer Science and Mathematics<br>"
+                + "- Deans List - Fall 2013, Spring 2014, Fall 2014, Spring 2015<br>"
+                + "- GPA: 3.75<br>"
+                + "- Major Courses: Data Structures, Machine Structure and Assembly Language, Web Programming, Web Engineering, Programming Languages, Operating Systems, Computation Theory, Networks, Computer System Security, Visualization, Natural Language Processing, Intro Electrical Systems, Intro Digital Electronics, Discrete Math, Multivariate Calculus, Differential Equations, Abstract Linear Algebra, Probability<br><br>"
                 + "Skills<br>"
-                + "Software Engineering, Object-Oriented Design, Functional Programming, Web Development, .NET<br><br>"
+                + "Programming Languages: C, C++, C#, Python, Javascript, HTML5, Clojure<br>"
+                + "Technologies: Django, Heroku, Amazon Web Services, jQuery, Node.js, XML, CSS, WPF, Bootstrap, Semantic-UI, PostgreSQL, UML, LaTeX<br>"
+                + "Software: Git, Clearcase, Vim, Emacs, VirtualBox, gdb, Visual Studio<br><br>"
                 + "Employment<br>"
-                + "Fairfield Auction, LLC. Monroe, Connecticut, 2009-2013<br>"
-                + "&nbsp;&nbsp;&nbsp;&nbsp;IT Support / Auction Internet Clerk<br>"
                 + "Teradyne, North Reading, Massachusetts, June 2014 - Present<br>"
-                + "&nbsp;&nbsp;&nbsp;&nbsp;Software Engineer Co-op - Semiconductor Test<br><br>"
+                + "- Software Engineer Co-op - Semiconductor Test<br>"
+                + "- Designed, implemented, and tested new driver language, customer-facing tool using WPF/.NET and producer/consumer pattern, and code-generation tools for regression testing and driver language generation"
+                + "- Identified and corrected bugs in core driver software"
+                + "- Configured and produced engineering process tools including programming style review, code-counting, and code-review<br><br>"
+                + "Fairfield Auction, LLC. Monroe, Connecticut, 2009-2013<br>"
+                + "- Rebuilt and maintained <a href='http://fairfieldauction.com'>company website</a>"
+                + "- Built and configured office workstations"
+                + "- Advised marketing strategy for transition to web-only auction platform"
                 + "Activities<br>"
                 + "Studied Guitar - 2005 - Present<br>"
                 + "&nbsp;&nbsp;&nbsp;&nbsp;Berklee College of Music Five-Week Summer Performance program - Guitar, Summer 2012<br>"
@@ -35,7 +42,7 @@ $(window).load(function() {
         $('.terminal-body').scrollTop(9007199254740992);
         $('#terminal-prompt-text').typed({
             strings: strings,
-            typeSpeed: 20,
+            typeSpeed: 5,
             backDelay: 0,
             loop: false,
             loopCount: false,
@@ -53,7 +60,7 @@ $(window).load(function() {
                 $('#terminal-text').html(terminalText);
                 $('#terminal-text').removeClass("invisible");
                 $('.terminal-body').scrollTop(9007199254740992);
-            }, 750);
+            }, 400);
         }
     }
 
