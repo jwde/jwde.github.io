2044b5ad8ebda402e8e1738d5f8e94d53bbdee5d
jwde <jaydestories@gmail.com>
2015-09-27 10:41:43 -0400

    Removed unused cv text from js

--- js/terminal.js
+++ js/terminal.js
@@ -11,26 +11,6 @@ $(window).load(function() {
                         "<a href='https://github.com/jwde/comp20/tree/master/mmap'>Marauder's Map</a> - Broadcast user locations to one another and display on a map.<br>" +
                         "<a href='https://github.com/jwde/comp20/tree/master/whereintheworld'>WhereInTheWorld</a> - Backend location tracker for Marauder's Map.<br>" +
                         "<a href='https://github.com/jwde/comp20/tree/master/security'>Security Analysis</a> of a classmate's website similar to Marauder's Map";
-    var cvText = "Academics<br>"
-                + "Tufts University - School of Engineering, Medford, MA, expected diploma May 2017<br>"
-                + "- Double-Majoring in Computer Science and Mathematics<br>"
-                + "- Deans List - Fall 2013, Spring 2014, Fall 2014, Spring 2015<br>"
-                + "- GPA: 3.75<br>"
-                + "- Major Courses: Data Structures, Machine Structure and Assembly Language, Web Programming, Web Engineering, Programming Languages, Operating Systems, Computation Theory, Networks, Computer System Security, Visualization, Natural Language Processing, Intro Electrical Systems, Intro Digital Electronics, Discrete Math, Multivariate Calculus, Differential Equations, Abstract Linear Algebra, Probability<br><br>"
-                + "Skills<br>"
-                + "Programming Languages: C, C++, C#, Python, Javascript, HTML5, Clojure<br>"
-                + "Technologies: Django, Heroku, Amazon Web Services, jQuery, Node.js, XML, CSS, WPF, Bootstrap, Semantic-UI, PostgreSQL, UML, LaTeX<br>"
-                + "Software: Git, Clearcase, Vim, Emacs, VirtualBox, gdb, Visual Studio<br><br>"
-                + "Employment<br>"
-                + "Teradyne, North Reading, Massachusetts, June 2014 - Present<br>"
-                + "- Software Engineer Co-op - Semiconductor Test<br>"
-                + "- Designed, implemented, and tested new driver language, customer-facing tool using WPF/.NET and producer/consumer pattern, and code-generation tools for regression testing and driver language generation<br>"
-                + "- Identified and corrected bugs in core driver software<br>"
-                + "- Configured and produced engineering process tools including programming style review, code-counting, and code-review<br><br>"
-                + "Fairfield Auction, LLC. Monroe, Connecticut, 2009-2013<br>"
-                + "- Rebuilt and maintained <a href='http://fairfieldauction.com'>company website</a><br>"
-                + "- Built and configured office workstations<br>"
-                + "- Advised marketing strategy for transition to web-only auction platform<br><br>";
     var contactText = "email: <a href='mailto:jaydestories@gmail.com'>jaydestories@gmail.com</a><br>" +
                       "<a href='https://www.linkedin.com/pub/jay-destories/105/399/75b'>LinkedIn</a>";
     function clearPrompt() {
@@ -72,17 +52,12 @@ $(window).load(function() {
         typeAnimated(["ls -l projects"], commandResponse(portfolioText));
     }
 
-    function cv() {
-        typeAnimated(["history"], commandResponse(cvText));
-    }
-
     function contact() {
         typeAnimated(["apropos contact jay"], commandResponse(contactText));
     }
 
     $('#aboutNav').on('click touchstart', about);
     $('#portfolioNav').on('click touchstart', portfolio);
-    //$('#cvNav').on('click touchstart', cv);
     $('#contactNav').on('click touchstart', contact);
 
     typeAnimated(["Hi there! My name is Jay and this is my website.", "cat Jay_DeStories"],
